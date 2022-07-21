02aecef1da83e739afccead26dc6e3752a67adc8
Rishi <grishi634@gmail.com>
2022-07-21 22:13:09 +0530

    added sudoku solver

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,6 +8,7 @@ import fs from "../../resources/foodstore.PNG";
 import cb from "../../resources/contestbuddy.PNG";
 import av from "../../resources/algorithmvisualiser.PNG";
 import rps from "../../resources/rps.PNG";
+import ss from "../../resources/sudokuSolver.jpg";
 const Projects = () => {
   let projectsList = [
     {
@@ -20,48 +21,57 @@ const Projects = () => {
       hosted: true,
     },
     {
-        name: "FoodBaZinga",
-        img: fs,
-        details:
+      name: "FoodBaZinga",
+      img: fs,
+      details:
         "This is Food ordering web app made using ReactJS,MongoDB and NodeJS",
-        link: "https://www.linkedin.com/posts/rishi-gupta-027298204_webdevelopment-learning-reactjs-activity-6814595111528353792-OW1H",
-        code: "https://github.com/rishigupta1109/FoodApp",
-        hosted: false,
+      link: "https://www.linkedin.com/posts/rishi-gupta-027298204_webdevelopment-learning-reactjs-activity-6814595111528353792-OW1H",
+      code: "https://github.com/rishigupta1109/FoodApp",
+      hosted: false,
     },
     {
-        name: "Corona Tracker",
-        img: ct,
-        details:
+      name: "Corona Tracker",
+      img: ct,
+      details:
         "This is a web application which shows stats of corona.It is made using ReactJS,CSS and covidAPI",
-        link: "https://rishigupta1109.github.io/CoronaTracker",
-        code: "https://github.com/rishigupta1109/CoronaTracker",
-        hosted: true,
+      link: "https://rishigupta1109.github.io/CoronaTracker",
+      code: "https://github.com/rishigupta1109/CoronaTracker",
+      hosted: true,
     },
     {
-        name: "Algorithm Visualiser",
-        img: av,
-        details: "This is a algorithm visualiser made using HTML,CSS and JS",
-        link: "https://rishigupta1109.github.io/algorithm_visualiser/",
-        code: "https://github.com/rishigupta1109/algorithm_visualiser",
-        hosted: true,
+      name: "Algorithm Visualiser",
+      img: av,
+      details: "This is a algorithm visualiser made using HTML,CSS and JS",
+      link: "https://rishigupta1109.github.io/algorithm_visualiser/",
+      code: "https://github.com/rishigupta1109/algorithm_visualiser",
+      hosted: true,
     },
     {
-        name: "Contest Buddy",
-        img: cb,
-        details:
+      name: "Sudoku Solver and Visualiser",
+      img: ss,
+      details:
+        "This is a sudoku solver and  visualiser made using ReactJS,CSS and JS",
+      link: "https://sudoku-solver-rose.vercel.app/",
+      code: "https://github.com/rishigupta1109/sudokuSolver",
+      hosted: true,
+    },
+    {
+      name: "Contest Buddy",
+      img: cb,
+      details:
         "This is a Chrome extension ehich shows stats of user on codeforces made using HTML,CSS,codeforcesAPI and JS",
-        link: "https://www.linkedin.com/posts/rishi-gupta-027298204_connections-chromeextension-competitiveprogramming-activity-6852170088955674624-XKie",
-        code: "https://github.com/rishigupta1109/contestBuddy",
-        hosted: false,
+      link: "https://www.linkedin.com/posts/rishi-gupta-027298204_connections-chromeextension-competitiveprogramming-activity-6852170088955674624-XKie",
+      code: "https://github.com/rishigupta1109/contestBuddy",
+      hosted: false,
     },
     {
-        name: "Rock Paper Scissors",
-        img: rps,
-        details:
+      name: "Rock Paper Scissors",
+      img: rps,
+      details:
         "This is a online multiplayer game made using html ,css, js,nodejs and socket.io",
-        link: "https://rishigupta1109.github.io/RockPaperScissor/",
-        code: "https://github.com/rishigupta1109/RockPaperScissor",
-        hosted: true,
+      link: "https://rishigupta1109.github.io/RockPaperScissor/",
+      code: "https://github.com/rishigupta1109/RockPaperScissor",
+      hosted: true,
     },
     {
       name: "Tic Tac Toe",
